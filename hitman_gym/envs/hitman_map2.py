937bb76a739ad31a8df6989c07b64752c2b77ab1
Youngrok Song <id2thomas@gmail.com>
2020-06-09 23:40:40 +0900

    fill map2

--- hitman_gym/envs/hitman_map2.py
+++ hitman_gym/envs/hitman_map2.py
@@ -12,14 +12,14 @@ MAP={
 	#-1: Out of Bounds
   "loc": [
     [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,2.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,1.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,1.,5.,1.,1.,0.,-1.],
+    [-1.,0.,1.,1.,4.,-1.,-1.,-1.],
+    [-1.,1.,3.,5.,1.,1.,-1.],
+    [-1.,1.,3.,5.,3.,2.,-1.],
+    [-1.,6.,1.,1.,1.,-1.,-1.],
     [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
     [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
   ],
-  # 1~16 : 4^2 
+  # 0~15 : 4^2 
   # 0: No connection
   # 1000: Up
   # 0100: Down
@@ -27,10 +27,10 @@ MAP={
   # 0001: Right
   "conn": [
     [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,4.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,12.,-1.,-1.,-1.,-1.,-1.],
-    [-1.,9.,3.,3.,3.,2.,-1.],
+    [-1.,5.,7.,3.,6.,-1.,-1.,-1.],
+    [-1.,12.,15.,12.,14.,3.,-1.],
+    [-1.,12.,15.,14.,13.,4.,-1.],
+    [-1.,9.,11.,3.,10.,-1.,-1.],
     [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
     [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
   ],
@@ -50,14 +50,79 @@ class HitmanMap1(gym.Env):
   #Hitman location & enemy location (map)
 
   def __init__(self):
-    self.map
+    self.map=np.zeros((7,7))
     self.action_space
 
+    self.dr=[-1,1,0,0]
+    self.dc=[0,0,-1,1]
+
+    self.enemies=[]
+    self.goal_loc=[2,1]
+
   def step(self, action):
-    pass
+    #check legal move
+    r_legal1=(self.cur_loc[0]+self.dr[action])>=7
+    r_legal2=(self.cur_loc[0]+self.dr[action])<0
+    c_legal1=(self.cur_loc[1]+self.dc[action])>=7
+    c_legal2=(self.cur_loc[1]+self.dc[action])<0
+    illegal=r_legal1|r_legal2|c_legal1|c_legal2
+    if illegal:
+      done=True
+      reward=-1
+    else:
+      #move
+      prev_r=self.cur_loc[0]
+      prev_c=self.cur_loc[1]
+
+      self.cur_loc[0]+=self.dr[action]
+      self.cur_loc[1]+=self.dc[action]
+      #default
+      reward=0
+      done=False
+
+      #1-check goal
+      if self.cur_loc[0]==self.goal_loc[0] and self.cur_loc[1]==self.goal_loc[1]:
+        reward=1
+        done=True
+        print('GOAL REACHED')
+      #2-check out of bounds
+      elif self.cur_state[0][self.cur_loc[0],self.cur_loc[1]]<0:
+        reward=-1
+        done=True
+      #3-perform
+      else:
+        for i in range(len(self.enemies)):
+          e=self.enemies[i]
+          #enemy caught
+          if e.check_range(self.cur_loc[0],self.cur_loc[1]):
+            done=True
+            reward=-1
+            break
+          elif e.check_caught(self.cur_loc[0],self.cur_loc[1]):
+            del self.enemies[i]
+            break
+
+        #move hitman
+        self.cur_state[0][prev_r,prev_c]=1
+        self.cur_state[0][self.cur_loc[0],self.cur_loc[1]]=0
+
+    return self.cur_state, reward, done, {}
     #return np.array(self.state), reward, done, {}
   def reset(self):
-    pass
+    #Reset Map
+    loc=np.array(MAP_a2['loc']) #(7,7)
+    conn=np.array(MAP_a2['conn']) #(7,7)
+    print('loc',loc.shape)
+    print('conn',conn.shape)
+    self.cur_state=np.stack([loc,conn],axis=0)
+
+    #Reset Positions
+    self.cur_loc=[4,5]
+
+    #Reset Enemies
+    self.enemies=[]
+    self.enemies.append(BlueEnemy(4,2,5))
+    return self.cur_state #(2,7,7)
     #return np.array(self.state)
   '''
   def render(self, mode='human'):
@@ -66,3 +131,17 @@ class HitmanMap1(gym.Env):
   '''
   def close(self):
     pass
+
+if __name__ == "__main__":
+    hm1=HitmanMap1()
+    print(hm1.map)
+    s=hm1.reset()
+    print(s.shape)
+    ans_path=[2,2,2,2,0,0]
+    for a in ans_path:
+      #self.cur_state, reward, done, {}
+      s,r,d,_=hm1.step(a)
+      print('Step {} Reward {} Pos{},{}'.format(a,r,hm1.cur_loc[0],hm1.cur_loc[1]))
+      print(hm1.cur_state[0])
+      if d:
+        break

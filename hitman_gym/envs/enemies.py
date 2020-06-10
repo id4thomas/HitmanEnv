8c9444e6155535ce1cefea00a5ce757471cf353c
Youngrok Song <id2thomas@gmail.com>
2020-06-10 23:37:14 +0900

    added check conn

--- hitman_gym/envs/enemies.py
+++ hitman_gym/envs/enemies.py
@@ -2,12 +2,13 @@
 
 
 class BlueEnemy:
-    def __init__(self, r, c, dir):
+    def __init__(self, r, c, dir, conn):
         # direction
         # location
         self.pos = [r, c]
         self.dir = dir - 3
-        self.conn = []
+        self.conn = "{0:4b}".format(int(conn))[-4:]
+        #print("conn str: {} conn {}".format(conn,self.conn))
         # Up,down,left,right
         self.check_r = [-1, 1, 0, 0]
         self.check_c = [0, 0, -1, 1]
@@ -16,7 +17,8 @@ class BlueEnemy:
         r_chk = (r - self.pos[0] == self.check_r[self.dir])
         c_chk = (c - self.pos[1] == self.check_c[self.dir])
         # Need to implement if connected check
-        return r_chk and c_chk
+        is_conn=self.conn[self.dir]=='1'
+        return r_chk and c_chk and is_conn
 
     def check_caught(self, r, c):
         return self.pos[0] == r and self.pos[1] == c

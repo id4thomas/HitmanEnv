c9660b0174da6323b976500bd8b351b146d6bb24
Youngrok Song <id2thomas@gmail.com>
2020-06-15 01:57:18 +0900

    add usage script

--- test_map3.py
+++ test_map3.py
@@ -3,13 +3,14 @@ import hitman_gym
 
 import os
 
-hm = gym.make('hitman-v4')
+hm = gym.make('hitman-v3')
 
 s = hm.reset('yellow')
 print(s.shape)
 # ans_path = [2, 2, 2, 2, 0, 0]
 #0 up 1 down 2 left 3 right
 ans_path = [3,1,1,3,3,2,2,3,2,3,3,0,0,3]
+print(hm.cur_state[0])
 for i in range(len(ans_path)):
     # self.cur_state, reward, done, {}
     a = ans_path[i]
